36911fc5aff768e744d40145a55f26bcd198862a
impzay <ipadgett@mail.umw.edu>
2026-09-20 20:07:18 -0400

    Add movement restriction signal functionality

--- garden-sprouts/plant.gd
+++ garden-sprouts/plant.gd
@@ -1,1 +1,15 @@
 extends Node2D
+
+signal mouse_entered_plant(plant);
+signal mouse_exited_plant(plant);
+
+func _ready():
+	var pollinator = get_parent().get_node("Pollinator")
+	mouse_entered_plant.connect(pollinator._on_mouse_entered_plant)
+	mouse_exited_plant.connect(pollinator._on_mouse_exited_plant)
+
+func _on_zone_mouse_entered() -> void:
+	mouse_entered_plant.emit(self)
+
+func _on_zone_mouse_exited() -> void:
+	mouse_exited_plant.emit(self)
--- garden-sprouts/pollinator.gd
+++ garden-sprouts/pollinator.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
+var mouse_hover_able_plant = null
 
 func _process(_delta: float) -> void:
 	return
 
 func move(new_pos):
 	#calculate a smooth path eventually
-	self.position = new_pos;
+	self.position = new_pos
 
 func _input(inp: InputEvent) -> void:
-	if inp is InputEventMouseButton and #able_plant is true:
+	if inp is InputEventMouseButton and mouse_hover_able_plant:
 		self.move(inp.position)
+
+func _on_mouse_entered_plant(plant):
+	print("moused")
+	mouse_hover_able_plant = plant;
+	
+func _on_mouse_exited_plant(plant):
+	mouse_hover_able_plant = null;
