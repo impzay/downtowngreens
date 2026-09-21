8cd2ce98bee939fef6f715ff53d0bca45e00e769
impzay <ipadgett@mail.umw.edu>
2026-09-20 21:13:45 -0400

    Add lerp smoothing for click movement, Add alpha transparency modulation on hovering state for plant scenes.

--- garden-sprouts/plant.gd
+++ garden-sprouts/plant.gd
@@ -2,14 +2,38 @@ extends Node2D
 
 signal mouse_entered_plant(plant);
 signal mouse_exited_plant(plant);
+var is_selected:bool = false;
+var time: float = 0.0;
+
+@onready var sprite = $Sprite2D
 
 func _ready():
 	var pollinator = get_parent().get_node("Pollinator")
 	mouse_entered_plant.connect(pollinator._on_mouse_entered_plant)
 	mouse_exited_plant.connect(pollinator._on_mouse_exited_plant)
 
+func _process(delta: float) -> void:
+	if is_selected:
+		time += delta
+		sprite.modulate.a = (sin(time * 3.0) + 1.0) / 4.0 + 0.5	
+		#sin gives you a range from -1 to 1
+		#time * 3.0 <- speed
+		#+1 gives you 0 to 1
+		#divide by 4 gives you 0 to 0.5
+		#+0.5 gives you 0.5 to 1
+func start_hovering():
+	time = 0.0;
+	is_selected = true
+	
+func stop_hovering():
+	is_selected = false
+	sprite.modulate.a = 1.0
+
+	
 func _on_zone_mouse_entered() -> void:
+	start_hovering();
 	mouse_entered_plant.emit(self)
 
 func _on_zone_mouse_exited() -> void:
+	stop_hovering();
 	mouse_exited_plant.emit(self)
--- garden-sprouts/pollinator.gd
+++ garden-sprouts/pollinator.gd
@@ -1,20 +1,26 @@
 extends Node2D
 
 var mouse_hover_able_plant = null
+var is_moving: bool = false
+var target_pos: Vector2 = Vector2.ZERO
 
-func _process(_delta: float) -> void:
-	return
+func _process(delta: float) -> void:
+	if is_moving:
+		position = position.lerp(target_pos, delta * 1.5)
+		
+	if position.distance_to(target_pos) < 2.0:
+		position = target_pos
+		is_moving = false	
 
 func move(new_pos):
-	#calculate a smooth path eventually
-	self.position = new_pos
+	target_pos = new_pos
+	is_moving = true
 
 func _input(inp: InputEvent) -> void:
 	if inp is InputEventMouseButton and mouse_hover_able_plant:
 		self.move(inp.position)
 
 func _on_mouse_entered_plant(plant):
-	print("moused")
 	mouse_hover_able_plant = plant;
 	
 func _on_mouse_exited_plant(plant):
